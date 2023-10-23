25c3fe5b8ea4f03b5b398afe6fa4d3771ca7c3f7
SamarthDevVaish <sammydv24@gmail.com>
2023-10-23 18:28:37 +1000

    Added references.

--- Chat-Feature/websocket/src/main/java/com/tupinamba/springbootwebsocket/SpringBootWebsocketApplication.java
+++ Chat-Feature/websocket/src/main/java/com/tupinamba/springbootwebsocket/SpringBootWebsocketApplication.java
@@ -1,3 +1,10 @@
+//code referenced from
+//https://gabrielpulga.medium.com/building-a-real-time-chat-application-with-spring-boot-and-websocket-4ad1c68ee217
+//and also
+//https://github.com/gabrielpulga/spring-boot-websocket
+
+//changed the language of the code to suit our needs.
+
 package com.tupinamba.springbootwebsocket;
 
 import org.springframework.boot.SpringApplication;
